e5588013e2b025415b41a26d7c22a5f1655767b9
quincyrchen <33114594+quincyrchen@users.noreply.github.com>
2020-01-25 17:55:07 -0800

    Set Volume Fee Max to 25% Buy

--- src/app/appraisal/appraisal.component.ts
+++ src/app/appraisal/appraisal.component.ts
@@ -11,13 +11,14 @@ export class AppraisalComponent implements OnInit {
   evepraisal_link;
   buybackpct = 0.85;
   iskperm3 = 350;
+  volumepctmax = 0.25;
 
   output=0;
   totalfeepct=0;
-  volumepctmax=25;
   buyfrombuyback=0;
   buyfeepct=0;
   items = [];
+  math = Math;
 
   constructor(private appraisalService : AppraisalService) { }
 
@@ -26,9 +27,9 @@ export class AppraisalComponent implements OnInit {
 
   executeAppraisal(): void {
     this.appraisalService.getAppraisal(this.evepraisal_link).subscribe(
-                    res=> {  this.output=res['totals']['buy']*this.buybackpct-Math.min(res['totals']['volume']*this.iskperm3, volumepctmax*res['totals']['buy']);
+                    res=> {  this.output=res['totals']['buy']*this.buybackpct-Math.min(res['totals']['volume']*this.iskperm3, this.volumepctmax*res['totals']['buy']);
                              this.totalfeepct=100.0*this.output/res['totals']['buy'];
-                             this.buyfrombuyback=res['totals']['buy']-Math.min(res['totals']['volume']*this.iskperm3, volumepctmax*res['totals']['buy']);
+                             this.buyfrombuyback=res['totals']['buy']-Math.min(res['totals']['volume']*this.iskperm3, this.volumepctmax*res['totals']['buy']);
                              this.buyfeepct=100.0*this.buyfrombuyback/res['totals']['buy'];
                              this.items = res['items'];},
                     res=> console.log(res));
